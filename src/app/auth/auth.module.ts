2dda3c6261e601decd38225f1a070c0c15b46207
Anderson Luís Furlan <alsfurlan@gmail.com>
2022-10-25 17:34:27 -0300

    Register module

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,16 @@
+import {CommonModule} from '@angular/common'
+import {NgModule} from '@angular/core'
+import {RouterModule} from '@angular/router'
+import {RegisterComponent} from './components/register/register.component'
+
+const routes = [
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+]
+@NgModule({
+  declarations: [RegisterComponent],
+  imports: [CommonModule, RouterModule.forChild(routes)],
+})
+export class AuthModule {}
