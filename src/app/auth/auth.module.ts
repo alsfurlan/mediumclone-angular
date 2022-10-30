ec878fc8d1434001084509c1d503d7147ce28b46
Anderson Luís Furlan <alsfurlan@gmail.com>
2022-10-30 12:51:01 -0300

    Creating auth interfaces

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,10 +1,10 @@
 import {CommonModule} from '@angular/common'
 import {NgModule} from '@angular/core'
 import {ReactiveFormsModule} from '@angular/forms'
-import {RouterModule} from '@angular/router'
+import {Route, RouterModule} from '@angular/router'
 import {RegisterComponent} from './components/register/register.component'
 
-const routes = [
+const routes: Route[] = [
   {
     path: 'register',
     component: RegisterComponent,
