472bdcc05f88673e84e6e836a718f56f5e3f9c33
Anderson Luís Furlan <alsfurlan@gmail.com>
2023-01-08 13:22:03 -0300

    Auth interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
-import {HttpClientModule} from '@angular/common/http'
+import {PersistanceService} from 'src/app/shared/services/persistance.service'
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
 import {NgModule} from '@angular/core'
 import {FormsModule, ReactiveFormsModule} from '@angular/forms'
 import {BrowserModule} from '@angular/platform-browser'
@@ -11,6 +12,7 @@ import {AppRoutingModule} from './app-routing.module'
 import {AppComponent} from './app.component'
 import {AuthModule} from './auth/auth.module'
 import {TopBarModule} from './shared/modules/top-bar/top-bar.module'
+import {AuthInterceptor} from './shared/services/auth-interceptor.service'
 
 @NgModule({
   declarations: [AppComponent],
@@ -29,7 +31,10 @@ import {TopBarModule} from './shared/modules/top-bar/top-bar.module'
     }),
     TopBarModule,
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
+    PersistanceService,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/shared/services/auth-interceptor.service.ts
@@ -0,0 +1,29 @@
+import {PersistanceService} from './persistance.service'
+import {Injectable} from '@angular/core'
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http'
+import {Observable} from 'rxjs'
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private persistanceService: PersistanceService) {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const token = this.persistanceService.get('accessToken')
+    if (token) {
+      req = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+    }
+    return next.handle(req)
+  }
+}
